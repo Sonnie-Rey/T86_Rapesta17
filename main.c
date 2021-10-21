49b7478ab2ee1a8108a631d2a3ce8dd0649983be
Sonnie Rey A. Rapesta <89230364+Sonnie-Rey@users.noreply.github.com>
2021-10-21 19:08:38 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+int main(){
+int choices, price, payment, change;
+  printf("Please select your favorite order");
+    printf("\n[1]Pepsi (P50) \n[2]Dr. Pepper (P100) \n[3]Coca-cola (P90) \nChoice:");
+    scanf("%d", &choices);
+    switch (choices){
 
-int main()
-{
-    int temp;
-    printf("\n Enter a temp:");
-    scanf("%d",&temp);
-
-    if (temp <0 )
-    {
-        printf("\n Freezing Weather \n");
-    }
-    else if (temp >=0 && temp <=10)
-    {
-        printf("\n Very Cold Weather \n");
-    }
-     else if (temp >=10 && temp <=20)
-    {
-        printf("\n Cold Weather \n");
-    }
-     else if (temp >=20 && temp <=30)
-    {
-        printf("\n Normal in Temperature \n");
-    }
-     else if (temp >=30 && temp <=40)
-    {
-        printf("\n Its Hot \n");
-    }
-     else if (temp >40)
-    {
-        printf("\n Its Very Hot \n");
-    }
-    return 0;
+    case 1:
+    price = 50;
+    printf("Please insert payment:");
+    scanf("%d", &payment);
+    if(payment>=price){
+    printf("Successfully Purchased! Enjoy your Pepsi Sir/Ma'am.\n");
+    change = 1111payment-price;
+    printf("\nChange: %d\n", change);
 }
+else{
+    printf("Insufficient Funds!");
+}
+    break;
 
+    case 2:
+    price = 100;
+    printf("Please insert payment:");
+    scanf("%d", &payment);
+    if(payment>=price){
+    printf("Successfully Purchased! Enjoy your Dr Pepper Sir/Ma'am.\n");
+    change = payment-price;
+    printf("\nChange: %d\n", change);
+}
+else{
+    printf("Insufficient funds!");
+}
+     break;
+
+
+     case 3:
+    price = 90;
+    printf("Please insert payment:");
+    scanf("%d", &payment);
+    if(payment>=price){
+    printf("Successfully Purchased! Enjoy your Cocacola Sir/Ma'am.\n");
+    change = payment-price;
+    printf("\nChange: %d\n", change);
+}
+else {
+  printf("Insufficient funds");
+}
+  break;
+   default:
+   printf("Selected item not found!");
+}
+return 0;}
