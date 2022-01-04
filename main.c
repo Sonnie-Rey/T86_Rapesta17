3197f4ee660629f97e3298dd7c3dad04463dbaea
Sonnie Rey A. Rapesta <89230364+Sonnie-Rey@users.noreply.github.com>
2022-01-04 11:53:55 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,11 @@
-#include<stdio.h>
+
+
+#include <stdio.h>
 int main(){
-int choices, price, payment, change;
-  printf("Please select your favorite order");
-    printf("\n[1]Pepsi (P50) \n[2]Dr. Pepper (P100) \n[3]Coca-cola (P90) \nChoice:");
-    scanf("%d", &choices);
-    switch (choices){
-
-    case 1:
-    price = 50;
-    printf("Please insert payment:");
-    scanf("%d", &payment);
-    if(payment>=price){
-    printf("Successfully Purchased! Enjoy your Pepsi Sir/Ma'am.\n");
-    change = 1111payment-price;
-    printf("\nChange: %d\n", change);
+    for(int x = 1; x<=9;x++){
+        for(int y = x;y<=9;y++){
+            y == 9 ? printf("%d\n", y) : printf("%d", y);
+        }
+    }
+    return 0;
 }
-else{
-    printf("Insufficient Funds!");
-}
-    break;
-
-    case 2:
-    price = 100;
-    printf("Please insert payment:");
-    scanf("%d", &payment);
-    if(payment>=price){
-    printf("Successfully Purchased! Enjoy your Dr Pepper Sir/Ma'am.\n");
-    change = payment-price;
-    printf("\nChange: %d\n", change);
-}
-else{
-    printf("Insufficient funds!");
-}
-     break;
-
-
-     case 3:
-    price = 90;
-    printf("Please insert payment:");
-    scanf("%d", &payment);
-    if(payment>=price){
-    printf("Successfully Purchased! Enjoy your Cocacola Sir/Ma'am.\n");
-    change = payment-price;
-    printf("\nChange: %d\n", change);
-}
-else {
-  printf("Insufficient funds");
-}
-  break;
-   default:
-   printf("Selected item not found!");
-}
-return 0;}
